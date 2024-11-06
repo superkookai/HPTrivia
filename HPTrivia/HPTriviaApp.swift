6b5291c6f626690049cf26e655d5f94846a95a3f
Weerawut Chaiyasomboon <weerawut@live.com>
2024-11-06 09:15:48 +0700

    Initial Commit

---
+++ HPTrivia/HPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPTriviaApp.swift
+//  HPTrivia
+//
+//  Created by Weerawut Chaiyasomboon on 6/11/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct HPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
